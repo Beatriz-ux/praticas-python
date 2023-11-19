d938e39a68bde83720194defa94b13403c74c1cb
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-11-19 14:38:20 -0300

    att conversao para octa e hexa exercicio3 P001

--- everaldina/P001/exercicio3.py
+++ everaldina/P001/exercicio3.py
@@ -1,6 +1,7 @@
 digitos = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-print("número | ASCII")
+print("número | ASCII | OCTA | HEXA")
 # printando numeros e seus respectivos valores em ASCII
 for i in digitos:
-    print(f" '{i}'  -   {ord(i)}")
+    ascci = ord(i)
+    print(f" '{i}'  -   {ascci}  -  {oct(ascci)} - {hex(ascci)}")
     
