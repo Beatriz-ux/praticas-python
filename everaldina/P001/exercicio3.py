e9bc9b841064f92c2e1cbfbb0aed6befc2ae7df9
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-11-19 14:26:08 -0300

    add questao 3 P001

---
+++ everaldina/P001/exercicio3.py
@@ -0,0 +1,6 @@
+digitos = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+print("número | ASCII")
+# printando numeros e seus respectivos valores em ASCII
+for i in digitos:
+    print(f" '{i}'  -   {ord(i)}")
+    
