251468ed926383768fa11fd645f148d29758f928
Caua-Clemente <cauaclementelima@gmail.com>
2023-11-23 17:14:10 -0300

    Subindo a primeira parte da pratica 002

---
+++ caua/p002/ToDoList1.py
@@ -0,0 +1,88 @@
+listaTarefas = []
+acao = 1;
+while(acao != 0):
+    print("\n---Menu de Acoes do ToDoList---")
+    print("1 - Registrar uma nova tarefa")
+    print("2 - Listar todas as tarefas")
+    print("3 - Marcar uma tarefa como concluida")
+    print("4 - Editar uma tarefa")
+    print("0 - Sair")
+    acao = input("Digite uma opcao: ")
+    acao = int(acao)
+
+    print()
+    match acao:
+        case 0:
+            print("---Finalizando operacao---\n")
+        
+        case 1:
+            print("---Registrando uma nova tarefa---")
+            descricao = input("Digite a descricao da tarefa: ").capitalize();
+            novaTarefa = "{}.{} [ ]".format((len(listaTarefas) + 1), descricao)
+            listaTarefas.append(novaTarefa);
+            print("Tarefa registrada com sucesso!")
+        
+        case 2:
+            print("--Lista de tarefas--")
+            if len(listaTarefas) == 0:
+                print("A lista de tarefas esta vazia")
+                exit;
+
+            for tarefa in listaTarefas:
+                    print(tarefa);
+        
+        case 3:
+
+            print("---Marcando uma tarefa---")   
+            index = -1         
+            check = False
+            id = input("Digite o id da tarefa: ");
+            for tarefa in listaTarefas:
+                index+=1
+                num = tarefa.split(".")[0]
+                if num == id:
+                    if tarefa[-3:] == "[x]":
+                        print("Essa tarefa ja foi concluida")
+                        check = True;
+                    else:
+                        listaTarefas[index] = tarefa.replace("[ ]", "[x]");
+                        print("Tarefa concluida com sucesso!")
+                        check = True;
+                    break;
+            
+            if check == False:
+                index = -1
+                print("Id nao encontrado")
+
+            #Colocando a tarefa para o inicio
+            if index > 0:
+                listaAux = listaTarefas.copy()
+                listaTarefas[0] = listaTarefas[index]
+                for i in range(index):
+                    listaTarefas[i+1] = listaAux[i]
+
+        
+        case 4:
+            print("---Editando uma tarefa---")
+            index = -1
+            check = False
+            id = input("Digite o id da tarefa: ");
+            for tarefa in listaTarefas:
+                index += 1
+                num = tarefa.split(".")[0]
+                status = tarefa[-3:]
+                if num == id:
+                    descricao = input("Digite a nova descricao da tarefa: ").capitalize()
+                    tarefa = "{}.{} {}".format(num, descricao, status)
+                    listaTarefas[index] = tarefa
+                    print("Tarefa editada com sucesso!")
+                    check = True;
+                    break;
+            
+            if check == False:
+                print("Id nao encontrado")
+        
+        case _:
+            print("--Erro--")
+            print("Opcao invalida, digite um numero de 0 a 4")
+    
