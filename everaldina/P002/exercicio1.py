28b8a3dfc780b998c37ae8b1e23f1bc0541cbccf
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-11-23 18:00:33 -0300

    fix: adicionando funçoes

--- everaldina/P002/exercicio1.py
+++ everaldina/P002/exercicio1.py
@@ -1,29 +1,67 @@
-def ordenar_tarefas(tarefa):
-    return tarefa.endswith('[x]')
-
 def listar_tarefas(lista):
-    lista.sort(key=ordenar_tarefas)
+    print(f"{'LISTA DE TAREFAS':-^20}")
     for tarefa in lista:
         if tarefa['finalizado']:
-            print(f"[x] {tarefa['descricao']}")
+            print(str(tarefa['id']) + '. ' + tarefa['descricao'] + ' [x]')
+        else:
+            print(str(tarefa['id']) + '. ' + tarefa['descricao'] + ' [ ]')
             
+# adiciona um item diciionário na lista
+# dicionario composta por id (int), descrição(string) e finalizado(bool)
 def adicionar_tarefa(lista):
-    descricao = input("Digite a descrição da tarefa: ")
+    descricao = input("\nDigite a descrição da tarefa: ")
     descricao = descricao.capitalize()
     
-    if len(lista) == 0:
-        id = 0
-    else:
-        id = lista[-1]['id'] + 1
-        
-    tarefa = str(id)+descricao+'[ ]'
-
-    print(tarefa)
+    # id é zero se a lista estiver vazia, se não, o id é o último id + 1
+    id = len(lista)+1
+    
+    # adiciona tarefa na lista
+    tarefa = {'id': id, 'descricao': descricao, 'finalizado': False}
     lista.append(tarefa)
     print("Tarefa adicionada com sucesso!")
     
+def concluir_tarefa(lista, id):
+    if(id <= len(lista)):
+        lista[id]['finalizado'] = True
+        print("\nTarefa concluída com sucesso!")
+    else:
+        print("\nTarefa não encontrada!")
+        
+def editar_tarefa(lista, id):
+    if(id <= len(lista)):
+        descricao = input("\nDigite a nova descrição da tarefa: ")
+        descricao = descricao.capitalize()
+        lista[id]['descricao'] = descricao
+        print("\nTarefa editada com sucesso!")
+    else:
+        print("\nTarefa não encontrada!")
+    
 def main():
     lista = []
+    while True:
+        print("1 - Listar tarefas")
+        print("2 - Adicionar tarefa")
+        print("3 - Concluir tarefa")
+        print("4 - Editar tarefa")
+        print("0. Sair")
+        opcao = int(input("Digite uma opção: "))
+        
+        match opcao:
+            case 1:
+                listar_tarefas(lista)
+            case 2:
+                adicionar_tarefa(lista)
+            case 3:
+                id = int(input("Digite o id da tarefa: "))
+                concluir_tarefa(lista, id)
+            case 4:
+                id = int(input("Digite o id da tarefa: "))
+                editar_tarefa(lista, id)
+            case 0:
+                break
+            case _default:
+                print("Opção inválida!")
+        print()
     
 
 if __name__ == '__main__':
