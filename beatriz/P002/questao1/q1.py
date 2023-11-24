8d2218c04eab8b12b2ebdf5806496bc32f0783de
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-11-23 21:30:48 -0300

    feat: função para buscar tarefa

---
+++ beatriz/P002/questao1/q1.py
@@ -0,0 +1,7 @@
+def buscarTarefa(listaDeTarefas, id):
+    posicao = 0
+    for cod in listaDeTarefas:
+        if int(cod.split("-")[0]) == id:
+            return posicao
+        posicao = posicao + 1
+    return -1
