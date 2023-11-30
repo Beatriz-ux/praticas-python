b727f12d90875c256c8b9d30f150bc2c53dd5a9e
Caua-Clemente <cauaclementelima@gmail.com>
2023-11-30 20:30:25 -0300

    Subind versao finalizada do exe1 da p003

--- caua/p003/exe1/main.py
+++ caua/p003/exe1/main.py
@@ -1,4 +1,4 @@
-from supermercado import Produto, Supermercado
+from supermercado import * 
 
 def main():
     pass
@@ -7,4 +7,37 @@ def main():
 if __name__ == "__main__":
     main()
 
+listaProdutos = []
+acao = ''
 
+while(acao != '0'):
+    print("\n---Menu do supermercado---")
+    print("1 - Inserir um novo produto")
+    print("2 - Excluir um produto cadastrado")
+    print("3 - Listar todos os produtos")
+    print("4 - Consultar o preco de um produto")
+    print("0 - Sair")
+    acao = input("Digite uma opcao: ")
+
+    print()
+    match acao:
+        case '0': 
+            print("---Finalizando sessao---")
+
+        case "1":
+            inserirProduto(listaProdutos)
+            
+        case '2':
+            excluirProduto(listaProdutos)
+            
+        case '3':
+            listarProduto(listaProdutos)
+            
+        case '4':
+            consultarProduto(listaProdutos)
+        
+        case _:
+            print("--Erro--")
+            print("Opcao invalida, digite um numero de 0 a 4")
+            
+            
--- caua/p003/exe1/supermercado.py
+++ caua/p003/exe1/supermercado.py
@@ -1,88 +1,105 @@
-class Produto:
-    codigo = ""
-    nome = ""
-    preco = 0.0
+def inserirProduto(lista):
+    print("---Inserindo um novo produto---")
+    nome = input("Nome: ")
+    codigo = input("Codigo (13 numeros): ")
+    if len(codigo) != 13 or (not codigo.isnumeric()):
+        print("Erro: o codigo deve conter apenas 13 caracteres numericos")
+        return
 
-    def __init__(self, nome, codigo, preco):
-        self.nome = nome
-        self.codigo = codigo
-        self.preco = preco
+    for i in lista:
+        if codigo == i['codigo']:
+            print("Erro: Codigo ja existe")
+            return
+
+    preco = input("Preco: ")
     
-class Supermercado:
-    produtos = []
-
-    def inserirProduto(self):
-        print("---Inserindo um novo produto---")
-        nome = input("Nome: ")
-        codigo = input("Codigo (13 numeros): ")
-        preco = float(input("Preco: "))
-
-        produto = Produto(nome.capitalize(), codigo, preco)
-        self.produtos.append(produto)
-        
-        print("Produto cadastrado com sucesso")
-
-
-    def excluirProduto(self):
-        if(self.produtos.count() == 0):
-            print("O estoque esta vazio")
-            return
-        
-        print("---Excluindo um produto---")
-        codigo = input("Codigo (13 numeros): ")
-
-        index = -1
-        check = False
-
-        for i in self.produtos:
-            index += 1
-            if(i.codigo == codigo):
-                check = True
-                break;
-        
-        if(check == False):
-            print("Codigo nao encontrado")
-            return
-        
-        self.produtos.remove(index)
-        print("Produto excluido com sucesso")
+    try:
+        preco = float(preco)
+    except:
+        print("Erro: Digite um valor valido")
+        return
     
+    if preco <= 0:
+        print("Erro: Digite um valor valido")
+        return
 
-    def listarProduto(self):
-        if(self.produtos.count() == 0):
-            print("O estoque esta vazio")
-            return
-        
-        print("---Listando Produtos---")
-
-        for i in self.produtos:
-            print("{} - {} - R${}".format(i.codigo, i.nome, format(i.preco, "2f")))
-
-
-    def consultarProduto(self):
-        if(self.produtos.count() == 0):
-            print("O estoque esta vazio")
-            return
-        
-        print("---Consultando um produto---")
-        codigo = input("Codigo (13 numeros): ")
-
-        index = -1
-        check = False
-
-        for i in self.produtos:
-            index += 1
-            if(i.codigo == codigo):
-                check = True
-                break;
-        
-        if(check == False):
-            print("Codigo nao encontrado")
-            return
-        
-        p = self.produtos[i]
-        print("{} - {} - R${}".format(p.codigo, p.nome, format(p.preco, "2f")))
+    produto = {"nome": nome.capitalize(), "codigo": codigo, "preco": preco} 
+    lista.append(produto)
     
+    print("Produto cadastrado com sucesso")
 
+
+def excluirProduto(lista):
+    if(len(lista) == 0):
+        print("O estoque esta vazio")
+        return
+    
+    print("---Excluindo um produto---")
+    codigo = input("Codigo (13 numeros): ")
+
+    if len(codigo) != 13 or (not codigo.isnumeric()):
+        print("Erro: o codigo deve conter apenas 13 caracteres numericos")
+        return
+    
+    index = -1
+    check = False
+
+    for i in lista:
+        index += 1
+        if(i['codigo'] == codigo):
+            check = True
+            break;
+    
+    if(check == False):
+        print("Codigo nao encontrado")
+        return
+    
+    print(index)
+    lista.pop(index)
+    print("Produto excluido com sucesso")
+
+
+def listarProduto(lista):
+    if(len(lista) == 0):
+        print("O estoque esta vazio")
+        return
+    
+    print("---Listando Produtos---")
+
+    qtde = 0
+    listaOrdenada = sorted(lista, key= lambda x: x['preco'])
+    for i in listaOrdenada:
+        if qtde%10 == 0 and qtde != 0:
+            print("--------------------------")
+        qtde+=1
+        print("{} - {} - R${}".format(i['codigo'], i['nome'], '{0:.2f}'.format(i["preco"])))
         
-        
+
+def consultarProduto(lista):
+    if(len(lista) == 0):
+        print("O estoque esta vazio")
+        return
+    
+    print("---Consultando um produto---")
+    codigo = input("Codigo (13 numeros): ")
+
+    if len(codigo) != 13 or (not codigo.isnumeric()):
+        print("Erro: o codigo deve conter apenas 13 caracteres numericos")
+        return
+    
+    index = -1
+    check = False
+
+    for i in lista:
+        index += 1
+        if(i['codigo'] == codigo):
+            check = True
+            break;
+    
+    if(check == False):
+        print("Codigo nao encontrado")
+        return
+    
+    p = lista[index]
+    print("{} - {} - R${}".format(p['codigo'], p['nome'], '{0:.2f}'.format(p["preco"])))
+
