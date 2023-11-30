8c7625fede069d848ac0b403d05882dca8428fe4
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-11-30 19:44:51 -0300

    feat: script exercicio2

---
+++ everaldina/P003/Empregados/main.py
@@ -0,0 +1,19 @@
+import json
+
+def reajusta_dez_porcento(empregados):
+    pass
+
+def carrega_empregados():
+    pass
+
+def salva_empregados(empregados):
+    pass
+
+def imprime_empregados(empregados):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
