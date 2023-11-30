d7e8883a66ed59688a14f850697308197d9c837d
João Manoel <jmaoliveira.cic@uesc.br>
2023-11-30 17:10:46 -0300

    fix

--- joao-manoel/p003/supermercado/main.py
+++ joao-manoel/p003/supermercado/main.py
@@ -1,33 +1,4 @@
-"""
-Faça um programa para um supermercado que permita a consulta de preço de produtos.
-O programa deverá armazenar de cada produto o seu código, seu nome e seu preço.
-    ○ Ao utilizar o programa o usuário deve poder:
-        1. Inserir um novo produto
-        2. Excluir um produto cadastrado
-        3. Listar todos os produtos com seus respectivos códigos e preços
-        4. Consultar o preço de um produto através de seu código.
-
-    O código deve ser formado de uma string numérica de 13 caracteres (pode conter zeros a esquerda, então não pode ser um número)
-
-    O nome pode ter qualquer tamanho e deve começar sempre com uma letra maiúscula.
-
-    O preço deve ser apresentado com duas casas decimais
-
-    O sistema deve listar os produtos na tela, 10 produtos de cada vez em ordem crescente de preço.
-
-    ● O código deve ser implementado numa pasta Supermercado e o programa principal deve ser implementado no arquivo main.py.
-
-    ● Pode utilizar como modelo o arquivo main.py disponível na postagem da atividade. Pesquise sobre esta estrutura e para que ela serve.
-
-    ● Cada uma das funcionalidades do menu de opções deve ser implementada numa função específica.
-
-    ● Já que não temos structs em Python vamos usar dicionários para armazenar cada produto.
-
-    ● Vamos armazenar os produtos numa lista de dicionários;
-"""
-
 import random
-
 
 def adcionarProduto(Produtos):
     print()
@@ -99,7 +70,7 @@ def main():
         produto = {'codigo': codigo, 'nome': nome, 'preco': float(preco)}
         addProduto(produto, Produtos)
 
-    
+    # Menu
     while True:
         print("1. Inserir um novo produto")
         print("2. Excluir um produto cadastrado")
