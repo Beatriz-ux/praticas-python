a9d39d94b5f951affe962474c10d624b147ed697
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-11-30 21:54:36 -0300

    feat: função de exibir menu e retorna a opção

---
+++ beatriz/P003/Supermercado/funcoes.py
@@ -0,0 +1,11 @@
+def listaMenu():
+    print("")
+    print("1 - Cadastrar Produto")
+    print("2 - Remover Produto")
+    print("3 - Listar Produtos")
+    print("4 - Consultar Preço")
+    print("0 - Sair")
+    print("")
+    print("Digite a opção desejada: ",end="")
+    opcao = int(input())
+    return opcao
