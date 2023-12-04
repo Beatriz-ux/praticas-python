732b6df910dceb262c21ffca17e70e92b536f21a
João Manoel <jmaoliveira.cic@uesc.br>
2023-12-03 22:07:22 -0300

    add listaNomes

--- P004/DataFruta.py
+++ P004/DataFruta.py
@@ -109,29 +109,84 @@ class ListaNomes(AnaliseDados):
         elementos vão existir na lista e depois
         solicita a digitação de cada um deles.
         '''
-        pass
+        print("------------------Entrada de nomes------------------")
+        try:
+            qtde = int(input("Quantidade de nomes a serem incluidos: "))
+
+            if qtde < 0:
+                raise ValueError("Quantidade inválida de nomes.")
+        except Exception:
+            print("Quantidade inválida de nomes.")
+            return
+
+        for i in range(qtde):
+            try:
+                nome = input(f"Digite o nome {i+1}: ")
+                self.__lista.append(nome)
+            except Exception:
+                print("Nome inválido.")
+                i -= 1
 
     def mostraMediana(self):
         '''
         Este método ordena a lista e mostra o
         elemento que está na metade da lista
         '''
-        pass    
+        if len(self.__lista) == 0:
+            print("Lista de datas vazia.")
+        else:
+            listaOrdenada = sorted(self.__lista)
+
+            tam = len(listaOrdenada)
+
+            if tam <= 0:
+                print("Lista de nomes vazia.")
+                return
+
+            if tam % 2 == 0:
+                mediana = listaOrdenada[(tam // 2) - 1]
+            else:
+                mediana = listaOrdenada[(tam // 2)]
+
+            print("Mediana de nomes: ", mediana)
 
     def mostraMenor(self):
         '''
         Este método retorna o menos elemento da lista
         '''
-        pass
+        if len(self.__lista) == 0:
+            print("Lista de nomes vazia.")
+        else:
+            menor = self.__lista[0]
+
+            for nome in self.__lista:
+                if nome < menor:
+                    menor = nome
+            
+            print("Menor nome: ", menor)
 
     def mostraMaior(self):
         '''
         Este método retorna o maior elemento da lista
         '''
-        pass    
+        if len(self.__lista) == 0:
+            print("Lista de nomes vazia.")
+        else:
+            maior = self.__lista[0]
+
+            for nome in self.__lista:
+                if nome > maior:
+                    maior = nome
+            
+            print("Maior nome: ", maior)
 
     def __str__(self):
-        pass
+        strLista = "--------Lista de Nomes--------\n"
+
+        for nome in self.__lista:
+            strLista += nome + "\n"
+        
+        return strLista
 	
 class ListaDatas(AnaliseDados):
         
@@ -302,14 +357,14 @@ class ListaIdades(AnaliseDados):
         pass
 
 def main():
-    #nomes = ListaNomes()
+    nomes = ListaNomes()
     datas = ListaDatas()
     #salarios = ListaSalarios()
     #idades = ListaIdades()
 
     #listaListas = [nomes, datas, salarios, idades]
-    #listaListas = [nomes]
-    #listaListas = [datas]
+    listaListas = [nomes]
+    listaListas = [datas]
     #listaListas = [salarios]
     #listaListas = [idades]
 
